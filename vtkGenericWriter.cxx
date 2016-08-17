e2473bb52d93fcaef5ebfa07701cc6926ee8fba9
Charles Gueunet <charles.gueunet@lip6.fr>
2016-08-17 21:20:04 +0200

    First version of the tee-like plugin
    With this plugin, you can make checkpoints of your data.
    For example, if you are debugging a plugin based on a random data-set
    input, launching the it over and over until explosion, this plugin
    will allow you to save on disk the state JUST BEFORE THE EXPLOSION!

---
+++ vtkGenericWriter.cxx
@@ -0,0 +1,115 @@
+#include "vtkGenericWriter.h"
+
+#include "vtkDataSet.h"
+#include "vtkDirectory.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+#include "vtksys/SystemTools.hxx"
+
+vtkStandardNewMacro(vtkGenericWriter);
+
+// Public
+// {{{
+
+//----------------------------------------------------------------------------
+void vtkGenericWriter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
+
+// }}}
+// Protected
+// {{{
+
+//----------------------------------------------------------------------------
+vtkGenericWriter::vtkGenericWriter()
+{
+    this->FileName = "";
+}
+
+//----------------------------------------------------------------------------
+vtkGenericWriter::~vtkGenericWriter()
+{
+}
+
+int vtkGenericWriter::FillOutputPortInformation(int port, vtkInformation* info)
+{
+  switch (port) {
+  case 0:
+    info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkDataSet");
+    break;
+  default:
+    info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkDataObject");
+    break;
+  }
+
+  return 1;
+}
+
+int vtkGenericWriter::RequestData(vtkInformation* request,
+                                  vtkInformationVector** inputVector,
+                                  vtkInformationVector* outputVector)
+{
+
+    vtkDataSet* inputData = vtkDataSet::GetData(inputVector[0]);
+
+    vtkInformation* outputInfo = outputVector->GetInformationObject(0);
+    vtkDataSet* outputData = vtkDataSet::SafeDownCast(outputInfo->Get(vtkDataObject::DATA_OBJECT()));
+    // this plugin does not modify the input
+    outputData->ShallowCopy(inputData);
+
+    if(FileName == ""){
+      // Default name
+      static unsigned occurenceNoName = 0;
+      FileName = "SavedData_" + std::to_string(occurenceNoName++);
+    } else {
+        vtkSmartPointer<vtkDirectory> dir=vtkSmartPointer<vtkDirectory>::New();
+        if(dir->FileIsDirectory(FileName.c_str())){
+           vtkErrorMacro("You should not pass a directory as output filename");
+           return 0;
+        }
+    }
+
+    // choose writer and save on disk
+    std::string fileExtension = vtksys::SystemTools::GetFilenameLastExtension(FileName);
+
+    // could use vtkDebugMacro
+#ifdef WRITER_DEBUG
+    if(fileExtension == ""){
+      std::cout << "no extension, will guess" << std::endl;
+    } else {
+      std::cout << "found extension : " << fileExtension << std::endl;
+    }
+#endif
+
+    if (fileExtension == "") {
+      if (inputData->GetDataObjectType() == VTK_UNSTRUCTURED_GRID) {
+        fileExtension = ".vtu";
+      } else if(inputData->GetDataObjectType() == VTK_IMAGE_DATA) {
+        fileExtension = ".vti";
+      } else if(inputData->GetDataObjectType() == VTK_POLY_DATA) {
+        fileExtension = ".vtp";
+      }
+      FileName += fileExtension;
+    }
+
+#ifdef WRITER_DEBUG
+    std::cout << "Data will be saved in : " << FileName << std::endl;
+#endif
+
+    if(fileExtension == ".vtu"){
+      WriteXMLFile<vtkXMLUnstructuredGridWriter>(inputData);
+    } else if (fileExtension == ".vti"){
+      WriteXMLFile<vtkXMLImageDataWriter>(inputData);
+    } else if (fileExtension == ".vtp"){
+      WriteXMLFile<vtkXMLPolyDataWriter>(inputData);
+    } else {
+      vtkErrorMacro("Unsupported DataType, nothing have been written!");
+      return 0;
+    }
+
+    return 1;
+}
+
+// }}}
---
+++ vtkGenericWriter.h
@@ -0,0 +1,66 @@
+// .NAME vtkGenericWriter - save a dataset on disc
+// .SECTION Description
+// vtkGenericWriter is a filter to save a dataset on the disk
+// while using it. A typical example is saving a plane with
+// random scalar-field when tracking a bug on a plugin.
+// It will allow you to keep the problematic data-set.
+
+#ifndef __vtkGenericWriter_h
+#define __vtkGenericWriter_h
+
+#include <string>
+
+#include "vtkDataSetAlgorithm.h"
+#include "vtkSmartPointer.h"
+
+// writers
+#include "vtkXMLImageDataWriter.h"
+#include "vtkXMLPolyDataWriter.h"
+#include "vtkXMLUnstructuredGridWriter.h"
+
+class VTK_EXPORT vtkGenericWriter : public vtkDataSetAlgorithm
+{
+public:
+  static vtkGenericWriter* New();
+  vtkTypeMacro(vtkGenericWriter, vtkDataSetAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkSetMacro(FileName, std::string);
+  vtkGetMacro(FileName, std::string);
+
+protected:
+  vtkGenericWriter();
+  ~vtkGenericWriter();
+
+  // VTK Interface
+
+  virtual int FillOutputPortInformation(int port, vtkInformation* info);
+
+  int RequestData(vtkInformation* request,
+                  vtkInformationVector** inputVector,
+                  vtkInformationVector* outputVector);
+
+  // methods
+  template <class TWriter>
+  void WriteXMLFile(vtkDataSet* inputData) const;
+
+  // fields
+
+  std::string FileName;
+
+private:
+  vtkGenericWriter(const vtkGenericWriter&);  // Not implemented.
+  void operator=(const vtkGenericWriter&);  // Not implemented.
+};
+
+// Template functions
+template <class TWriter> 
+void vtkGenericWriter::WriteXMLFile(vtkDataSet* inputData) const {
+  vtkSmartPointer<TWriter> writer=vtkSmartPointer<TWriter>::New();
+  writer->SetFileName(FileName.c_str());
+  writer->SetInputData(inputData);
+
+  writer->Write();
+}
+
+#endif
